f40b857641fb91533bb5367b6183a9a786cf3fa2
WeesELBio <161979348+WeesELBio@users.noreply.github.com>
2026-01-20 21:36:50 +0000

    Update app.py

--- app.py
+++ app.py
@@ -13,17 +13,33 @@ def index():
     return render_template("index.html")
 
 
+@app.route("/get_structure")
+def get_structure():
+    structure = {}
+
+    for q in QUESTIONS:
+        s = q["subject"]
+        t = q["topic"]
+        st = q["subtopic"]
+
+        structure.setdefault(s, {})
+        structure[s].setdefault(t, set())
+        structure[s][t].add(st)
+
+    for s in structure:
+        for t in structure[s]:
+            structure[s][t] = list(structure[s][t])
+
+    return jsonify(structure)
+
+
 @app.route("/get_questions", methods=["POST"])
 def get_questions():
     data = request.json
     selected = []
 
     for q in QUESTIONS:
-        if (
-            q["subject"] in data["subjects"] and
-            q["topic"] in data["topics"] and
-            q["subtopic"] in data["subtopics"]
-        ):
+        if q["subtopic"] in data["subtopics"]:
             selected.append(q)
 
     random.shuffle(selected)
